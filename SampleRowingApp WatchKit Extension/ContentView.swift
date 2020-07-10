826f2ed75fe3cd7c08464f18d2ef4467b40aa464
Rohini <rohini.vaidya@parjanya.org>
2020-07-10 14:12:41 +0530

    Initial Commit

---
+++ SampleRowingApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleRowingApp WatchKit Extension
+//
+//  Created by Rohini Vaidya on 7/10/20.
+//  Copyright © 2020 Rohini. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleRowingApp WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  SampleRowingApp WatchKit Extension
+//
+//  Created by Rohini Vaidya on 7/10/20.
+//  Copyright © 2020 Rohini. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
